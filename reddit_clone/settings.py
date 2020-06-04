8376e25f1fb0762032981071a84edd58c6e11676
Mary Waithera <njihiamary11@gmail.com>
2020-06-04 16:22:06 +0300

    Create registration and login features to facilitate the registration of new users and to ensure that existing and registered users can access the site

--- reddit_clone/settings.py
+++ reddit_clone/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'posts.apps.PostsConfig',
     'bootstrap4',
+    'django_registration',
 ]
 
 MIDDLEWARE = [
--- reddit_clone/urls.py
+++ reddit_clone/urls.py
@@ -19,4 +19,6 @@ from django.urls import path, include
 urlpatterns = [
     path('', include('posts.urls')),
     path('admin/', admin.site.urls),
+    path('accounts/', include('django_registration.backends.one_step.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
